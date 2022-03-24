b202be25de3034965f2dfaec3bb7d4f3724a1fa9
Luke Anderson <lukeqanderson@gmail.com>
2022-03-24 17:06:36 +0900

    Update mailing service

--- node_backend/public/script.js
+++ node_backend/public/script.js
@@ -11,7 +11,7 @@ const navDropDown = () => {
 /* Adds responsive class condense navbar after a selection is made*/
 const navUp = () => {
   const topnav = document.getElementById("myTopnav");
-  if (topnav.className === "topnav sticky responsive" ) {
+  if (topnav.className === "topnav sticky responsive") {
     topnav.className = "topnav sticky";
   } else {
     topnav.className = "topnav sticky responsive";
@@ -22,12 +22,12 @@ const navUp = () => {
 const showSlides = (n) => {
   let i;
   const slides = document.getElementsByClassName("projectSlide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
 // for slide show 
@@ -47,7 +47,7 @@ const navIcon = document.getElementById('smallNavIcon');
 navIcon.addEventListener("click", navDropDown);
 
 // add event lisetner for closing navbar on item selection
-const navBarItems =  document.getElementsByClassName("navlink navitem");
+const navBarItems = document.getElementsByClassName("navlink navitem");
 for (let i = 0; i < navBarItems.length; i++) {
   navBarItems[i].addEventListener("click", navUp);
 }
@@ -56,48 +56,6 @@ for (let i = 0; i < navBarItems.length; i++) {
 const prevButton = document.getElementById("prev-button");
 const nextButton = document.getElementById("next-button");
 
-prevButton.addEventListener("click",minusSlides);
-nextButton.addEventListener("click",plusSlides);
+prevButton.addEventListener("click", minusSlides);
+nextButton.addEventListener("click", plusSlides);
 
-// variable to store the contact form
-const form = document.querySelector("form");
-
-// adds event lister for submit button click
-form.addEventListener("submit", (e) => {
-  // to prevent the page refreshing
-  e.preventDefault;
-
-  // to select input elements from form 
-  let firstName = document.getElementById("fname");
-  let lastName = document.getElementById("lname");
-  let email = document.getElementById("email");
-  let message = document.getElementById("message");
-
-  // creates an object with the input elements
-  let contactData = {
-    firstName: firstName.value,
-    lastName: lastName.value,
-    email: email.value,
-    message: message.value
-  }
-
-  // to send form data to the backend using AJAX
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST', '/');
-  xhr.setRequestHeader('content-type', 'application/json');
-  xhr.onload = () => {
-    console.log(xhr.responseText);
-    if (xhr.responseText === 'success') {
-      alert("Message sent. Thank you!");
-      firstName.value = '';
-      lastName.value = '';
-      email.value = '';
-      message.value = '';
-    }
-    else {
-      alert("Message failed to send. Please email me at lukeqanderson@gmail.com");
-    }
-  }
-
-  xhr.send(JSON.stringify(contactData));
-})
